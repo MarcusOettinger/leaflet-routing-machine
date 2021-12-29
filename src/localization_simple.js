4b080766c0bdeaded84fd8116329a16c1ddb85c6
Marcus Oettinger <Marcus@Oettinger-physics.de>
2021-12-29 19:35:19 +0100

    Create localization_simple.js
    localization.js on a diet...

---
+++ src/localization_simple.js
@@ -0,0 +1,181 @@
+/* localization_simple.js: siplest form - just de/en as I will only use those
+ * Wed 29 Dec 19:34:28 CET 2021
+ */
+/* 
+   NOTICE
+   Since version 3.2.5, the functionality in this file is by
+   default NOT used for localizing OSRM instructions.
+   Instead, we rely on the module osrm-text-instructions (https://github.com/Project-OSRM/osrm-text-instructions/).
+   
+   This file can still be used for other routing backends, or if you specify the
+   stepToText option in the OSRMv1 class.
+*/
+
+(function() {
+	'use strict';
+
+	L.Routing = L.Routing || {};
+
+	var Localization = L.Class.extend({
+		initialize: function(langs) {
+			this._langs = L.Util.isArray(langs) ? langs.slice() : [langs, 'en'];
+
+			for (var i = 0, l = this._langs.length; i < l; i++) {
+				var generalizedCode = /([A-Za-z]+)/.exec(this._langs[i])[1]
+				if (!Localization[this._langs[i]]) {
+					if (Localization[generalizedCode]) {
+						this._langs[i] = generalizedCode;
+					} else {
+						throw new Error('No localization for language "' + this._langs[i] + '".');
+					}
+				}
+			}
+		},
+
+		localize: function(keys) {
+			var dict,
+				key,
+				value;
+
+			keys = L.Util.isArray(keys) ? keys : [keys];
+
+			for (var i = 0, l = this._langs.length; i < l; i++) {
+				dict = Localization[this._langs[i]];
+				for (var j = 0, nKeys = keys.length; dict && j < nKeys; j++) {
+					key = keys[j];
+					value = dict[key];
+					dict = value;
+				}
+
+				if (value) {
+					return value;
+				}
+			}
+		}
+	});
+
+	module.exports = L.extend(Localization, {
+		'en': {
+			directions: {
+				N: 'north',
+				NE: 'northeast',
+				E: 'east',
+				SE: 'southeast',
+				S: 'south',
+				SW: 'southwest',
+				W: 'west',
+				NW: 'northwest',
+				SlightRight: 'slight right',
+				Right: 'right',
+				SharpRight: 'sharp right',
+				SlightLeft: 'slight left',
+				Left: 'left',
+				SharpLeft: 'sharp left',
+				Uturn: 'Turn around'
+			},
+			instructions: {
+				// instruction, postfix if the road is named
+				'Head':
+					['Head {dir}', ' on {road}'],
+				'Continue':
+					['Continue {dir}'],
+				'TurnAround':
+					['Turn around'],
+				'WaypointReached':
+					['Waypoint reached'],
+				'Roundabout':
+					['Take the {exitStr} exit in the roundabout', ' onto {road}'],
+				'DestinationReached':
+					['Destination reached'],
+				'Fork': ['At the fork, turn {modifier}', ' onto {road}'],
+				'Merge': ['Merge {modifier}', ' onto {road}'],
+				'OnRamp': ['Turn {modifier} on the ramp', ' onto {road}'],
+				'OffRamp': ['Take the ramp on the {modifier}', ' onto {road}'],
+				'EndOfRoad': ['Turn {modifier} at the end of the road', ' onto {road}'],
+				'Onto': 'onto {road}'
+			},
+			formatOrder: function(n) {
+				var i = n % 10 - 1,
+				suffix = ['st', 'nd', 'rd'];
+
+				return suffix[i] ? n + suffix[i] : n + 'th';
+			},
+			ui: {
+				startPlaceholder: 'Start',
+				viaPlaceholder: 'Via {viaNumber}',
+				endPlaceholder: 'End'
+			},
+			units: {
+				meters: 'm',
+				kilometers: 'km',
+				yards: 'yd',
+				miles: 'mi',
+				hours: 'h',
+				minutes: 'min',
+				seconds: 's'
+			}
+		},
+
+		'de': {
+			directions: {
+				N: 'Norden',
+				NE: 'Nordosten',
+				E: 'Osten',
+				SE: 'Südosten',
+				S: 'Süden',
+				SW: 'Südwesten',
+				W: 'Westen',
+				NW: 'Nordwesten',
+				SlightRight: 'leicht rechts',
+				Right: 'rechts',
+				SharpRight: 'scharf rechts',
+				SlightLeft: 'leicht links',
+				Left: 'links',
+				SharpLeft: 'scharf links',
+				Uturn: 'Wenden'
+			},
+			instructions: {
+				// instruction, postfix if the road is named
+				'Head':
+					['Richtung {dir}', ' auf {road}'],
+				'Continue':
+					['Geradeaus Richtung {dir}', ' auf {road}'],
+				'SlightRight':
+					['Leicht rechts abbiegen', ' auf {road}'],
+				'Right':
+					['Rechts abbiegen', ' auf {road}'],
+				'SharpRight':
+					['Scharf rechts abbiegen', ' auf {road}'],
+				'TurnAround':
+					['Wenden'],
+				'SharpLeft':
+					['Scharf links abbiegen', ' auf {road}'],
+				'Left':
+					['Links abbiegen', ' auf {road}'],
+				'SlightLeft':
+					['Leicht links abbiegen', ' auf {road}'],
+				'WaypointReached':
+					['Zwischenhalt erreicht'],
+				'Roundabout':
+					['Nehmen Sie die {exitStr} Ausfahrt im Kreisverkehr', ' auf {road}'],
+				'DestinationReached':
+					['Sie haben ihr Ziel erreicht'],
+				'Fork': ['An der Kreuzung {modifier}', ' auf {road}'],
+				'Merge': ['Fahren Sie {modifier} weiter', ' auf {road}'],
+				'OnRamp': ['Fahren Sie {modifier} auf die Auffahrt', ' auf {road}'],
+				'OffRamp': ['Nehmen Sie die Ausfahrt {modifier}', ' auf {road}'],
+				'EndOfRoad': ['Fahren Sie {modifier} am Ende der Straße', ' auf {road}'],
+				'Onto': 'auf {road}'
+			},
+			formatOrder: function(n) {
+				return n + '.';
+			},
+			ui: {
+				startPlaceholder: 'Start',
+				viaPlaceholder: 'Via {viaNumber}',
+				endPlaceholder: 'Ziel'
+			}
+		}
+
+	});
+})();
